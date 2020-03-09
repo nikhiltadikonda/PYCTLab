23addde1536d9bc0600585e490297e6f86a4f6a6
Nikhil Tadikonda <nikhiltadikonda@gmail.com>
2020-03-09 12:06:34 +0530

    Changes

---
+++ Python Files/DataStructuresInPython/numpy.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 24 08:52:59 2019
+
+@author: cvr
+"""
+
+import numpy as np
+import pandas as pd
+
+arr1=np.array([[1,2,3],
+             [4,5,6],
+             [7,8,9]])
+#print(arr1)
+
+
+arr2=np.array([[3,2,1],
+             [6,5,4],
+             [9,8,7]])
+#print(arr1*arr2)
+
+sample1=pd.read_csv("test.csv")
+
